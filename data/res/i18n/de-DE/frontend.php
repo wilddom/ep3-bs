4619cf2edeb5b26ce2f431964429b617e4fc59a6
Dominik Wild <dominik.wild@gmail.com>
2021-07-03 21:24:39 +0200

    shorter translations

--- data/res/i18n/de-DE/frontend.php
+++ data/res/i18n/de-DE/frontend.php
@@ -25,7 +25,7 @@ return [
 
     'Administration' => 'Verwaltung',
 
-    'My bookings' => 'Meine Buchungen',
-    'My account' => 'Meine Daten',
+    'My bookings' => 'Buchungen',
+    'My account' => 'Konto',
 
 ];
